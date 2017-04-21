30ca3a2edc4e5204b0d1b0fe3d68fe1a9af30f79
System Administrator <root@Itohans-MacBook-Pro.local>
2017-04-21 02:42:37 -0400

    looking good

--- lab4Library.py
+++ lab4Library.py
@@ -1,184 +1,261 @@
 import numpy as np
+from copy import deepcopy
+from operator import add
 
 
 #Define your xprime as a function
-def xVal(paraList):
-	x = paraList[0]
-	t = paraList[1]
-	dxdt = (4*(np.exp(0.8*t))) - (0.5*x)
+def xVal(InitialVal, t0):
+	#x = paraList[0]
+	x = InitialVal
+	dxdt = (4*(np.exp(0.8*t0))) - (0.5*x)
 
 	return dxdt
 
 
-'''@param: xPrime the function for the xPrime equations
-   @param: xPara is a list of inputs  to the xPrime functions
+'''@param: xEqn is the function  for the equation of the ODE
+   @param: xParaInitial is a list of inputs  to the xEqn functions
+   @param: t0 is the value of t at time 0
+   @param: h is the value of the step size to be used
    @param: adaptBit -> set to 0
    @return: returns a list of the values of the slope using forward
    			euler method
 '''
-def fEuler(xPrime, xPara, h, adaptBit):
+def fEuler(xEqn, xParaInitial, t0, h, adaptBit):
 
-	xEqn = xPrime 
-	slope =  xEqn(xPara)
+	 
+	slope =  xEqn(xParaInitial, t0)
 	
 	return slope
 
 
 
-'''@param: xPrime the function for the xPrime equations
-   @param: xPara is a list of inputs  to the xPrime functions
+'''@param: xEqn is the function for the equation of the ODE
+   @param: xParaInitial is a list of inputs to the xEqn functions
+   @param: t0 is the value of t at time 0
+   @param: h is the value of the step size to be used
    @param: adaptBit -> set to 0
-   @return: returns a list of the values of the slope using trapezoidal
+   @return: returns the values of the slope using trapezoidal
    			euler method
 '''
-def tEuler(xPrime, xPara, h, adaptBit):	
-	
-	xEqn = xPrime  
-			
-	oldX = xPara[0]
-	oldT = xPara[1]
-	slope0 =  xEqn(xPara)
-	newX = oldX + slope0*h
-	newT = oldT + 1
-	slope1 = xEqn([newX, newT])
-	realSlope = (slope0 + slope1)/2
-		
+def tEuler(xEqn, xParaInitial, t0, h, adaptBit):
+
+
+	slope0 = xEqn(xParaInitial, t0)	
+	t1 = t0 + h
+	if not isinstance(xParaInitial, list):
+		newParas = xParaInitial + slope0*h
+		slope1 = xEqn(newParas, t1)
+		realSlope = (slope0 + slope1)/2
+
+
+	else:
+		slope0h = list(map(lambda x: x*h, slope0))
+		newParas = 	map(add, xParaInitial, slope0h)
+		slope1 = xEqn(newParas, t1)
+		realSlope = map(add, slope0, slope1)
+		realSlope = list(map(lambda x: x/2, realSlope))
+
 	return realSlope
 
 
 
-'''@param: xPrime is a list of functions for the xPrime equations
-   @param: xPara is a list of lists of inputs  to the xPrime functions
+'''@param: xEqn is the function for the equation of the ODE
+   @param: xParaInitial is a list of inputs to the xEqn functions
+   @param: t0 is the value of t at time 0
+   @param: h is the value of the step size to be used
    @param: adaptBit -> set to 0
-   @return: returns a list of the values of the slope using backward
+   @return: returns the values of the slope using backward
    			euler method
-'''	
-def bEuler(xPrime, xPara, h, adaptBit):	
-	
-	xEqn = xPrime  #Get the eqn for each dependent variable
-	xEqnPara = xPara #Get the parameters for each equation
-	
-	oldX = xEqnPara[0]
-	oldT = xEqnPara[1]
-	slope0 =  xEqn(xEqnPara)
-	newX = oldX + slope0*h
-	newT = oldT + 1
-	
-	slope1 = xEqn([newX, newT])
-	realSlope = slope1
-	
-	return realSlope	
+'''
+def bEuler(xEqn, xParaInitial, t0, h, adaptBit):
+
+	slope0 = xEqn(xParaInitial, t0)	
+	t1 = t0 + h
+	if not isinstance(xParaInitial, list):
+		newParas = xParaInitial + slope0*h
+		realSlope = xEqn(newParas, t1)
 
 
+	else:
+		slope0h = list(map(lambda x: x*h, slope0))
+		newParas = 	map(add, xParaInitial, slope0h)
+		realSlope = xEqn(newParas, t1)
+		
+	return realSlope
 
-'''@param: xPrime is a list of functions for the xPrime equations
-   @param: xPara is a list of lists of inputs  to the xPrime functions
-   @param: h is the step size, should be floating point
-   @param: adaptBit- 0 = no time adaptation, 1 = time adaptation
+
+'''@param: xEqn is the function for the equation of the ODE
+   @param: xParaInitial is a list of inputs to the xEqn functions
+   @param: t0 is the value of t at time 0
+   @param: h is the value of the step size to be used
+   @param: adaptBit-> 0 = no time adaptation, 1 = time adaptation
    @return: returns a list of the values of the slope using RK34
    			method and no time adaptation
 '''	
-def rk4(xPrime, xPara, h, adaptBit):
-	
-	xEqn = xPrime #Get the eqn for each dependent variable
-	xEqnPara = xPara 
-	xK1 = xEqnPara[0]
-	tk1 = xEqnPara[1]
+def rk4(xEqn, xParaInitial, t0, h, adaptBit):
 
-	k1 = xEqn(xEqnPara)
-
-	xK2 = xK1 + ((k1*h)/2)
-	tk2 = tk1 + (h/2)
-
-	k2 = xEqn([xK2, tk2])
-	
-	xK3 = xK1 + ((k2*h)/2)
+	tk2 = t0 + (h/2)
 	tk3 = tk2
-	k3 = xEqn([xK3, tk3])
+	tk4 = t0 + h
 
-	xK4 = xK1 + (k3*h)
-	tk4 = tk1 + h	
-	k4 = xEqn([xK4, tk4])	
+	# print "t0 %f "  %t0
+	# print "tK2 %f "  %tk2
+	# print "tK3 %f "  %tk3
+	# print "tK4 %f "  %tk4
+	# print "\n"
 
-	Err = ((-5*k1 + 6*k2 + 8*k3 - 9*k4)*h)/72
+	if not isinstance(xParaInitial, list):
+		xK1 = xParaInitial
+	
+		k1 = xEqn(xParaInitial, t0)
 
-	slope = (k1 + 2*k2 + 2*k3 + k4)/6	
+	 	xK2 = xK1 + ((k1*h)/2)
+	 	#tk2 = t0 + (h/2)
 
-	if not adaptBit:	
-		return slope
+		k2 = xEqn(xK2, tk2)
+	
+		xK3 = xK1 + ((k2*h)/2)
+		#tk3 = tk2
+		k3 = xEqn(xK3, tk3)
+
+		xK4 = xK1 + (k3*h)
+		#tk4 = tk1 + h	
+		k4 = xEqn(xK4, tk4)	
+
+		Err = ((-5*k1 + 6*k2 + 8*k3 - 9*k4)*h)/72
+
+		realSlope = (k1 + 2*k2 + 2*k3 + k4)/6	
+
+
+
+		# print "K1 %d "  %k1
+		# print "K2 %d "  %k2
+		# print "K3 %d "  %k3
+		# print "K4 %d "  %k4
+
 
 	else:
-		return [slope, Err]		
+		
+		k1List = xEqn(xParaInitial, t0)
+		k1Mod = list(map(lambda x: ((x*h)/2), k1List))
+		
+		xParaK2 = map(add, xParaInitial, k1Mod)
+		k2List = xEqn(xParaK2, tk2)
+		k2Mod = list(map(lambda x: ((x*h)/2), k2List))
+
+		xParaK3 = map(add, xParaInitial, k2Mod)
+		k3List = xEqn(xParaK3, tk3)
+		k3Mod = list(map(lambda x: (x*h), k3List))
+
+		xParaK4 = map(add, xParaInitial, k3Mod)
+		k4List = xEqn(xParaK4, tk4)
+
+
+		#Caluculating slope using the k values
+		k1Slope = list(map(lambda x: ((x/6)), k1List))
+		k2Slope = list(map(lambda x: ((x*2)/6)), k2List)
+		k3Slope = list(map(lambda x: ((x*2)/6)), k3List)
+		k4Slope = list(map(lambda x: ((x/6)), k4List))
+
+		sum1 = map(add, k1Slope, k2Slope)
+		sum2 = map(add, sum1, k3Slope)
+		realSlope = map(add, sum2, k4Slope) 
+
+		#Caluculating Err using the k values
+		k1Err = list(map(lambda x: (-5*x), k1List))
+		k2Err = list(map(lambda x: (6*x), k2List))
+		k3Err = list(map(lambda x: (8*x), k3List))
+		k4Err = list(map(lambda x: (-9*x), k4List))
+
+		err1 = map(add, k1Err, k2Err)
+		err2 = map(add, err1, k3Err)
+		err3 = map(add, err2, k4Err)
+
+		Err = list(map(lambda x: ((h*x)/72), err3))
+
+
+	
+	if not adaptBit:	
+		return realSlope
+
+	else:
+		return [realSlope, Err]		
+
+
+
+
 
 
 '''@param: odeMethod is the odeMethod to be used
-   @param: eqnList is a list of functions for the xPrime equations
-   @param: eqnPara is a list of lists of inputs  to the xPrime functions
-   @param: h is the step size, should be floating point
-   @param: tol is the tolerance if time adaptation is used
+   @param: xEqn is the function for the equation of the ODE
+   @param: xParaInitial is a list of inputs to the xEqn functions
+   @param: t0 is the value of t at time 0
+   @param: h is the value of the step size to be used
+   @param: tol1 is the tolerance1 if time adaptation is used
+   @param: tol2 is the tolerance2 if time adaptation is used
    @param: adaptBit -> 1 = adaptation, 0 = no adaptation
-   @param: timeRange tells how many times to use assuming timestep of 1
-   @return: returns a list of the values of the slope using RK34
-   			method with time adaptation
+   @param: timeStop is the last time value to be used
+   @return: returns a list of the calculated parameter values
 '''	
 
-def odeSolver(odeMethod, eqnList, eqnPara, h, tol1, tol2 , adaptBit , timeRange):
-	nDependent = len(eqnList) #number of dependent variables
-	dependentList = [] #List of lists of final values for dependent
-	
-	
-	for dependent in range(nDependent):
-		xPrime = eqnList[dependent]
-		xPara = eqnPara[dependent]
-		numPara = len(xPara)
-		#t0 = xPara[numPara - 1] #t0 is always last in the list
-		xList = []
-		#xList.append(xPara)
-		
-		for time in range(1,timeRange + 1):
-			if not adaptBit:
-				#print 'no adapt'
-				slope = odeMethod(xPrime, xPara, h, adaptBit)
+def odeSolver(odeMethod, xEqn, xParaInitial, t0, h, tol1, tol2 ,
+ adaptBit , timeStop):
 
-				x = xPara[0]
-				xList.append(x)
-				x = x + slope*h
-				
-				xPara = [x, time]
+	finalValues = []
+
+	#bEuler(xEqn, xParaInitial, t0, h, adaptBit)
+	timeList = np.arange(t0, (timeStop), h)
+
+	#for time in range((t0 + 1), len(timeList)):
+	for time in range(t0, len(timeList)):
+		t0 = timeList[time]
+
+		if not adaptBit:
+			slope = odeMethod(xEqn, xParaInitial, t0, h, adaptBit)
+			if not isinstance(xParaInitial, list):
+				finalValues.append(xParaInitial)
+				newX = xParaInitial + slope*h
+				xParaInitial = newX
 
 			else:
-				result = odeMethod(xPrime, xPara, h, adaptBit)
-				slope = result[0]
-				x = xPara[0]
-				xList.append(x)
-				x = x + slope*h
+				finalValues.append(xParaInitial)
+				slope0h = list(map(lambda x: x*h, slope))
+				newParas = map(add, xParaInitial, slope0h)
+				xParaInitial = newParas
+		
+		elif adaptBit:
+		
+			result = odeMethod(xEqn, xParaInitial, t0, h, adaptBit)
+			slope = result[0]
+			if not isinstance(xParaInitial, list):
+				finalValues.append(xParaInitial)
+				newX = xParaInitial + slope*h
+				xParaInitial = newX
 
-				E	= result[1]
-				Err = abs(E)/abs(x)	
+			else:
+				finalValues.append(xParaInitial)
+				slope0h = list(map(lambda x: x*h, slope))
+				newParas = map(add, xParaInitial, slope0h)
+				xParaInitial = newParas
 
-				if Err > tol1:
+			
+			E	= result[1]
+			Err = abs(E)/abs(xParaInitial)
+
+			if Err > tol1:
 					
-					h = h/2
+				h = h/2
 				
-				elif Err < tol2:
+			elif Err < tol2:
 					
-					h = 2*h
+				h = 2*h
 
-				elif tol2 <Err < tol1:
-					h = h	
-
-				xPara = [x, time]	
-
-
-		dependentList.append(xList)
-
+			elif tol2 <Err < tol1:
+				h = h			
 		
 
 
-
-	return dependentList	
-
-	#if adaptBit:
+	return finalValues
 
 
 
@@ -190,29 +267,19 @@ def odeSolver(odeMethod, eqnList, eqnPara, h, tol1, tol2 , adaptBit , timeRange)
 
 
 
-
-xList = []
-xPrime = [xVal]
-xPara = [[2.0,0]]
-xList.append(xPara[0])
-slope = []
+odeMethod = rk4
+xEqn = xVal
+xParaInitial = 2.0
+t0 = 0
+h = 1.0
 tol1 = 1e-1
 tol2 = 1e-3
+adaptBit = 1
+timeStop =5
 
-'''for t in range(1,5):
-	
 
-	
-	slopeVal = (rk4(xPrime,xPara,1.0))
-
-	x = xPara[0] + slopeVal*1
-#print x 
-	xPara = [x,t]
-	xList.append(x)'''
-
-#(odeMethod, eqnList, eqnPara, h, tol1, tol2 , adaptBit , timeRange):
-xList = odeSolver(rk4,xPrime,xPara, 1.0, tol1, tol2, 1, 5)
-
+xList = odeSolver(odeMethod, xEqn, xParaInitial, t0, h, tol1, tol2 ,
+ adaptBit , timeStop)
 
 print  xList
 
