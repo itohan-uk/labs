8e4230d9af23011a91c9a78eea830fb2dd5d7b4a
System Administrator <root@Itohans-MacBook-Pro.local>
2017-04-21 04:41:14 -0400

    most working

--- lab4Library.py
+++ lab4Library.py
@@ -1,15 +1,96 @@
 import numpy as np
 from copy import deepcopy
 from operator import add
+from numpy import linalg as la
 
 
 #Define your xprime as a function
+
+'''@param: InitialVal is the initial parameter value at t0
+   @param: t0 is the time at t0
+   @return: ode value
+'''
 def xVal(InitialVal, t0):
-	#x = paraList[0]
 	x = InitialVal
 	dxdt = (4*(np.exp(0.8*t0))) - (0.5*x)
 
 	return dxdt
+
+
+
+'''@param: InitialVal is a list of all the values to be passed to the eqn
+		   V1 and V2 are the first and second term in the list
+		   R1, R2, R3 are the 3rd, 4th and 5th terms respectively
+		   C1, C2 are the 6th and 7th terms
+		   i(t)List is the 8th term in the list and is a list of all the 
+		   current at various times
+		   timeIdx is the 9th term in the list used to index  i(t)List
+   @param: t0 is the time
+'''
+'''@param: InitialVal is a list of all the values to be passed to the eqn
+		   V1 and V2 are the first and second term in the list
+   @param: t0 is the time
+   @return: vector for the slopes of V1 and V2
+'''
+def circuitOde(initialVal, t0):
+	V1 = initialVal[0]
+	V2 = initialVal[1]
+	m = (0.1e-3)/(1e-9)
+	global R1
+	global R2
+	global R3
+	global C1
+	global C2
+
+	if ((t0 == 0) or (t0 in np.arange(11e-9, 20e-9, 1e-9)) or
+		(t0 in np.arange(31e-9, 40e-9, 1e-9)) or 
+		(t0 in np.arange(51e-9, 60e-9, 1e-9)) or
+		(t0 in np.arange(71e-9, 80e-9, 1e-9)) or
+		(t0 in np.arange(91e-9, 100e-9, 1e-9))):
+
+		i = 0.0
+
+	elif ((t0 in np.arange(1e-9, 10e-9, 1e-9)) or 
+		(t0 in np.arange(21e-9, 30e-9, 1e-9)) or
+		(t0 in np.arange(41e-9, 50e-9, 1e-9)) or
+		(t0 in np.arange(61e-9, 70e-9, 1e-9)) or
+		(t0 in np.arange(81e-9, 90e-9, 1e-9))):
+		i = 0.1e-3
+
+	else:
+		i = m*t0	
+
+
+	# R1 = initialVal[2]
+	# R2 = initialVal[3]
+	# R3 = initialVal[4]
+	# C1 = initialVal[5]
+	# C2 = initialVal[6]
+	#i = initialVal[7]	
+	#iList = initialVal[7]
+	#timeIdx = initialVal[8]
+	#i = iList[timeIdx]
+
+	A  = -((1/(C1*R1)) + (1/(C1*R2)))
+	B  = 1/(C1*R2)
+	C = 1/(C2*R2)
+	D = -((1/(C2*R2)) + (1/(C2*R3)))
+
+	mat = [[A, B], [C, D]]
+
+	vec = [V1, V2]
+	
+
+	addition = [(i/C1), 0]
+
+	#Solving the matrix with 
+	Mat = np.array(mat)
+	Vec = np.array(vec)
+
+	dV1dV2dt = (la.solve(Mat, Vec) + np.array(addition))
+
+	return dV1dV2dt
+
 
 
 '''@param: xEqn is the function  for the equation of the ODE
@@ -38,7 +119,6 @@ def fEuler(xEqn, xParaInitial, t0, h, adaptBit):
    			euler method
 '''
 def tEuler(xEqn, xParaInitial, t0, h, adaptBit):
-
 
 	slope0 = xEqn(xParaInitial, t0)	
 	t1 = t0 + h
@@ -84,6 +164,7 @@ def bEuler(xEqn, xParaInitial, t0, h, adaptBit):
 	return realSlope
 
 
+
 '''@param: xEqn is the function for the equation of the ODE
    @param: xParaInitial is a list of inputs to the xEqn functions
    @param: t0 is the value of t at time 0
@@ -106,20 +187,15 @@ def rk4(xEqn, xParaInitial, t0, h, adaptBit):
 
 	if not isinstance(xParaInitial, list):
 		xK1 = xParaInitial
-	
 		k1 = xEqn(xParaInitial, t0)
 
 	 	xK2 = xK1 + ((k1*h)/2)
-	 	#tk2 = t0 + (h/2)
-
 		k2 = xEqn(xK2, tk2)
 	
 		xK3 = xK1 + ((k2*h)/2)
-		#tk3 = tk2
 		k3 = xEqn(xK3, tk3)
 
 		xK4 = xK1 + (k3*h)
-		#tk4 = tk1 + h	
 		k4 = xEqn(xK4, tk4)	
 
 		Err = ((-5*k1 + 6*k2 + 8*k3 - 9*k4)*h)/72
@@ -153,8 +229,8 @@ def rk4(xEqn, xParaInitial, t0, h, adaptBit):
 
 		#Caluculating slope using the k values
 		k1Slope = list(map(lambda x: ((x/6)), k1List))
-		k2Slope = list(map(lambda x: ((x*2)/6)), k2List)
-		k3Slope = list(map(lambda x: ((x*2)/6)), k3List)
+		k2Slope = list(map(lambda x: ((x*2)/6), k2List))
+		k3Slope = list(map(lambda x: ((x*2)/6), k3List))
 		k4Slope = list(map(lambda x: ((x/6)), k4List))
 
 		sum1 = map(add, k1Slope, k2Slope)
@@ -180,7 +256,6 @@ def rk4(xEqn, xParaInitial, t0, h, adaptBit):
 
 	else:
 		return [realSlope, Err]		
-
 
 
 
@@ -265,18 +340,22 @@ def odeSolver(odeMethod, xEqn, xParaInitial, t0, h, tol1, tol2 ,
 
 
 
+R1 = R2 = R3 = 10e3
+C1 = C2 = 1e-12
 
 
 odeMethod = rk4
-xEqn = xVal
-xParaInitial = 2.0
+#xEqn = xVal
+xEqn = circuitOde
+#xParaInitial = 2.0
+xParaInitial = [0,0]
 t0 = 0
-h = 1.0
+h = 0.2e-9
 tol1 = 1e-1
 tol2 = 1e-3
 adaptBit = 1
-timeStop =5
-
+#timeStop =5
+timeStop = 100e-9
 
 xList = odeSolver(odeMethod, xEqn, xParaInitial, t0, h, tol1, tol2 ,
  adaptBit , timeStop)
